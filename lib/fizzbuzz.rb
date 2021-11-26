30e2bac65f0cfcdf493f2fb75a534685728f31a2
OrganicAC <anthonychaplin17@gmail.com>
2021-11-26 16:27:49 +0000

    altered fb.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(number)
-  if number == 1
-    1
-  elsif number == 3
-    "fizz"
-  elsif number == 5
-    "buzz"
-  elsif number == 15
-    "fizzbuzz"
+for i in 1..100 do
+    if ((i % 5) == 0) && ((i % 3) == 0)
+      puts "FizzBuzz"
+    elsif (i % 5) == 0
+      puts "Buzz"
+    elsif (i % 3) == 0
+      puts "Fizz"
+    else
+      puts "#{i}"
+    end
   end
-end
